ad837894bc9af43ef2b1f0691d791a9739326523
x9 <xonika9@gmail.com>
2023-11-17 10:35:21 +0300

    commit

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -2,25 +2,20 @@
 
 import { CustomerField } from '@/app/lib/definitions';
 import Link from 'next/link';
-import {
-    CheckIcon,
-    ClockIcon,
-    CurrencyDollarIcon,
-    UserCircleIcon,
-} from '@heroicons/react/24/outline';
+import { CheckIcon, ClockIcon, CurrencyDollarIcon, UserCircleIcon } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
 import { createInvoice } from '@/app/lib/actions';
+import { useFormState } from 'react-dom';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
+    const initialState = { message: null, errors: {} };
+    const [state, dispatch] = useFormState(createInvoice, initialState);
     return (
-        <form action={createInvoice}>
+        <form action={dispatch}>
             <div className="rounded-md bg-gray-50 p-4 md:p-6">
                 {/* Customer Name */}
                 <div className="mb-4">
-                    <label
-                        htmlFor="customer"
-                        className="mb-2 block text-sm font-medium"
-                    >
+                    <label htmlFor="customer" className="mb-2 block text-sm font-medium">
                         Choose customer
                     </label>
                     <div className="relative">
@@ -29,6 +24,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                             name="customerId"
                             className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
                             defaultValue=""
+                            aria-describedby="customer-error"
                         >
                             <option value="" disabled>
                                 Select a customer
@@ -41,14 +37,19 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                         </select>
                         <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
                     </div>
+                    <div id="customer-error" aria-live="polite" aria-atomic="true">
+                        {state.errors?.customerId &&
+                            state.errors.customerId.map((error: string) => (
+                                <p className="mt-2 text-sm text-red-500" key={error}>
+                                    {error}
+                                </p>
+                            ))}
+                    </div>
                 </div>
 
                 {/* Invoice Amount */}
                 <div className="mb-4">
-                    <label
-                        htmlFor="amount"
-                        className="mb-2 block text-sm font-medium"
-                    >
+                    <label htmlFor="amount" className="mb-2 block text-sm font-medium">
                         Choose an amount
                     </label>
                     <div className="relative mt-2 rounded-md">
@@ -60,17 +61,24 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                                 step="0.01"
                                 placeholder="Enter USD amount"
                                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                                aria-describedby="amount-error"
                             />
                             <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+                        </div>
+                        <div id="amount-error" aria-live="polite" aria-atomic="true">
+                            {state.errors?.amount &&
+                                state.errors.amount.map((error: string) => (
+                                    <p className="mt-2 text-sm text-red-500" key={error}>
+                                        {error}
+                                    </p>
+                                ))}
                         </div>
                     </div>
                 </div>
 
                 {/* Invoice Status */}
                 <fieldset>
-                    <legend className="mb-2 block text-sm font-medium">
-                        Set the invoice status
-                    </legend>
+                    <legend className="mb-2 block text-sm font-medium">Set the invoice status</legend>
                     <div className="rounded-md border border-gray-200 bg-white px-[14px] py-3">
                         <div className="flex gap-4">
                             <div className="flex items-center">
@@ -80,6 +88,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                                     type="radio"
                                     value="pending"
                                     className="h-4 w-4 border-gray-300 bg-gray-100 text-gray-600 focus:ring-2 focus:ring-gray-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-gray-600"
+                                    aria-describedby="status-error"
                                 />
                                 <label
                                     htmlFor="pending"
@@ -104,6 +113,14 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                                 </label>
                             </div>
                         </div>
+                    </div>
+                    <div id="amount-error" aria-live="polite" aria-atomic="true">
+                        {state.errors?.status &&
+                            state.errors.status.map((error: string) => (
+                                <p className="mt-2 text-sm text-red-500" key={error}>
+                                    {error}
+                                </p>
+                            ))}
                     </div>
                 </fieldset>
             </div>
